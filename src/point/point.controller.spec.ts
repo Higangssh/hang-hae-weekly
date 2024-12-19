3af58bc3c1294a707be879924eda947b0d75fb17
SangheeSon <sonsanghee3@gmail.com>
2024-12-19 17:11:42 +0900

    fix: 존재하지 않는 유저는 없다는 가정으로 변경

--- src/point/point.controller.spec.ts
+++ src/point/point.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { PointController } from './point.controller';
 import { PointService } from './point.service';
 
--- src/point/point.controller.ts
+++ src/point/point.controller.ts
@@ -61,7 +61,7 @@ export class PointController {
     if (amount <= 0) {
       throw new BadRequestException('Amount must be greater than 0');
     }
-    return this.pointService.chargeUserPoint(userId, amount, Date.now());
+    return this.pointService.chargeUserPoint(userId, amount);
   }
 
   /**
@@ -84,6 +84,6 @@ export class PointController {
     if (amount <= 0) {
       throw new BadRequestException('Amount must be greater than 0');
     }
-    return this.pointService.useUserPoint(userId, amount, Date.now());
+    return this.pointService.useUserPoint(userId, amount);
   }
 }
